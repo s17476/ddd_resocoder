5c7ab9c42599dc3a26c006a3ba94961727002f78
Grzegorz Fraczek <s17476@pjwstk.edu.pl>
2022-12-12 09:56:08 +0100

    Lesson 17 - Create, Update, Delete Notes

--- lib/infrastructure/notes/note_repository.dart
+++ lib/infrastructure/notes/note_repository.dart
@@ -20,21 +20,61 @@ class Noterepository implements INoteRepository {
   });
 
   @override
-  Future<Either<NoteFailure, Unit>> create(Note note) {
-    // TODO: implement create
-    throw UnimplementedError();
+  Future<Either<NoteFailure, Unit>> create(Note note) async {
+    try {
+      final userDoc = await firebaseFirestore.userDocument();
+      final noteDto = NoteDto.fromDomain(note);
+
+      await userDoc.noteCollection.doc(noteDto.id).set(noteDto.toJson());
+
+      return right(unit);
+    } on PlatformException catch (e) {
+      if (e.message!.contains('PREMISSION_DENIED')) {
+        return left(const NoteFailure.insufficientPremission());
+      } else {
+        return left(const NoteFailure.unexpected());
+      }
+    }
   }
 
   @override
-  Future<Either<NoteFailure, Unit>> delete(Note note) {
-    // TODO: implement delete
-    throw UnimplementedError();
+  Future<Either<NoteFailure, Unit>> delete(Note note) async {
+    try {
+      final userDoc = await firebaseFirestore.userDocument();
+      final noteId = note.id.getOrCrasch();
+
+      await userDoc.noteCollection.doc(noteId).delete();
+
+      return right(unit);
+    } on PlatformException catch (e) {
+      if (e.message!.contains('PREMISSION_DENIED')) {
+        return left(const NoteFailure.insufficientPremission());
+      } else if (e.message!.contains('NOT_FOUND')) {
+        return left(const NoteFailure.unableToUpdate());
+      } else {
+        return left(const NoteFailure.unexpected());
+      }
+    }
   }
 
   @override
-  Future<Either<NoteFailure, Unit>> update(Note note) {
-    // TODO: implement update
-    throw UnimplementedError();
+  Future<Either<NoteFailure, Unit>> update(Note note) async {
+    try {
+      final userDoc = await firebaseFirestore.userDocument();
+      final noteDto = NoteDto.fromDomain(note);
+
+      await userDoc.noteCollection.doc(noteDto.id).update(noteDto.toJson());
+
+      return right(unit);
+    } on PlatformException catch (e) {
+      if (e.message!.contains('PREMISSION_DENIED')) {
+        return left(const NoteFailure.insufficientPremission());
+      } else if (e.message!.contains('NOT_FOUND')) {
+        return left(const NoteFailure.unableToUpdate());
+      } else {
+        return left(const NoteFailure.unexpected());
+      }
+    }
   }
 
   @override
